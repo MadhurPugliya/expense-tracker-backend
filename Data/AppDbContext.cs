305ff2588b2df7efb65fe2eab0de90d18f387326
MadhurPugliya <madhurpugliya10@gmail.com>
2026-02-23 16:23:58 +0530

    Phase1

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,5 +14,24 @@ namespace ExpenseTracker.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Wallet> Wallets { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // User to Categories relationship (One-to-Many)
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Categories)
+                .WithOne(c => c.User)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // User to Wallets relationship (One-to-Many)
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Wallets)
+                .WithOne(w => w.User)
+                .HasForeignKey(w => w.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
